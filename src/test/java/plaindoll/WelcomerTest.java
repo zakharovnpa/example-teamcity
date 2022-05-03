c2f6429dba9584e48da7b64b8a16b48002225f04
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2022-05-03 17:37:38 +0400

    Create WelcomerTest.java

---
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -0,0 +1,29 @@
+package plaindoll;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WelcomerTest {
+	
+	private Welcomer welcomer = new Welcomer();
+
+	@Test
+	public void welcomerSaysWelcome() {
+		assertThat(welcomer.sayWelcome(), containsString("Welcome"));
+	}
+	@Test
+	public void welcomerSaysFarewell() {
+		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
+	}
+	@Test
+	public void welcomerSaysHunter() {
+		assertThat(welcomer.sayWelcome(), containsString("hunter"));
+		assertThat(welcomer.sayFarewell(), containsString("hunter"));
+	}
+	@Test
+	public void welcomerSaysStatus() {
+		assertThat(welcomer.sayStatus(), containsString("status"));
+	}
+}
