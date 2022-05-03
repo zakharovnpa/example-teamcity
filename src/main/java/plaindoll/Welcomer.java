cde289ce6695303d4df7446b22987a234dfb788f
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2022-05-03 17:36:50 +0400

    Create Welcomer.java

---
+++ src/main/java/plaindoll/Welcomer.java
@@ -0,0 +1,13 @@
+package plaindoll;
+
+public class Welcomer{
+	public String sayWelcome() {
+		return "Welcome home, good hunter. What is it your desire?";
+	}
+	public String sayFarewell() {
+		return "Farewell, good hunter. May you find your worth in waking world.";
+	}
+	public String sayStatus() {
+		return "Hello! My status is Good";
+	}
+}
